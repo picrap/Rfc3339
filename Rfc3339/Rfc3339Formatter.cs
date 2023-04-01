931ce26e208395febc4e5397d122607a650f3962
pascal <pascal@craponne.fr>
2023-04-02 00:54:49 +0200

    Add project files.

---
+++ Rfc3339/Rfc3339Formatter.cs
@@ -0,0 +1,73 @@
+﻿using System.Text;
+
+namespace Rfc3339;
+
+public class Rfc3339Formatter : IFormatProvider, ICustomFormatter
+{
+    public string Format(string? format, object? arg, IFormatProvider? formatProvider)
+    {
+        return arg switch
+        {
+            null => "",
+            DateTime dateTime => Format(dateTime, format),
+            DateTimeOffset dateTimeOffset => Format(dateTimeOffset, format),
+            _ => throw new NotSupportedException($"Can’t format type {arg.GetType()}")
+        };
+    }
+
+    public object? GetFormat(Type? formatType)
+    {
+        if (formatType == typeof(DateTime) || formatType == typeof(DateTime?)
+                                           || formatType == typeof(DateTimeOffset) || formatType == typeof(DateTimeOffset?))
+            return this;
+        return null;
+    }
+
+    public static string Format(DateTime dateTime, string? format)
+    {
+        return Format(dateTime, DateTimeOffset.Now.Offset, format);
+    }
+
+    public static string Format(DateTimeOffset dateTime, string? format)
+    {
+        return Format(dateTime.DateTime, dateTime.Offset, format);
+    }
+
+    private static string Format(DateTime dateTime, TimeSpan offset, string? format = null)
+    {
+        var builder = new StringBuilder();
+        builder.AppendFormat("{0:yyyy-MM-ddThh:mm:ss}", dateTime);
+        var totalNs = dateTime.Ticks % 10_000_000 * 100;
+        if (totalNs > 0)
+        {
+            builder.AppendFormat(".{0:000}", totalNs / 1_000_000);
+            var μs = totalNs % 1_000_000;
+            if (μs > 0)
+            {
+                builder.AppendFormat("{0:000}", μs / 1_000);
+                var ns = μs % 1000;
+                if (ns > 0)
+                    builder.AppendFormat("{0:000}", ns);
+            }
+        }
+
+        switch (dateTime.Kind)
+        {
+            case DateTimeKind.Unspecified:
+                break;
+            case DateTimeKind.Utc:
+                builder.Append("Z");
+                break;
+            case DateTimeKind.Local:
+                if (offset < TimeSpan.Zero)
+                    builder.AppendFormat("-{0:hh:mm}", -offset);
+                else
+                    builder.AppendFormat("+{0:hh:mm}", offset);
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+
+        return builder.ToString();
+    }
+}
---
+++ Rfc3339Test/ToStringTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NUnit.Framework;
+using Rfc3339;
+
+namespace Rfc3339Test;
+
+[TestFixture]
+public class ToStringTests
+{
+    [Test]
+    public void SimpleDateTimeTest()
+    {
+        var s = Rfc3339Formatter.Format(new DateTime(2000, 12, 02, 3, 4, 5, DateTimeKind.Utc), null);
+        Assert.AreEqual("2000-12-02T03:04:05Z", s);
+    }
+
+    [Test]
+    public void SimpleDateTimeMsTest()
+    {
+        var s = Rfc3339Formatter.Format(new DateTime(2000, 12, 02, 3, 4, 5, DateTimeKind.Utc)
+                                        + TimeSpan.FromMilliseconds(300), null);
+        Assert.AreEqual("2000-12-02T03:04:05.300Z", s);
+    }
+
+    [Test]
+    public void SimpleDateTimeμsTest()
+    {
+        var s = Rfc3339Formatter.Format(new DateTime(2000, 12, 02, 3, 4, 5, DateTimeKind.Utc)
+                                        + TimeSpan.FromMilliseconds(0.9), null);
+        Assert.AreEqual("2000-12-02T03:04:05.000900Z", s);
+    }
+
+    [Test]
+    public void SimpleDateTimeNsTest()
+    {
+        var ts = TimeSpan.FromTicks(3);
+        var s = Rfc3339Formatter.Format(new DateTime(2000, 12, 02, 3, 4, 5, DateTimeKind.Utc) + ts, null);
+        Assert.AreEqual("2000-12-02T03:04:05.000000300Z", s);
+    }
+}
